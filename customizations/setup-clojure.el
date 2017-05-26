45e285ccd1522006d92cb46171ebea1f29298df2
Dejan Josifovic <dejan.josifovic@igt.com>
2017-05-26 16:42:33 +0200

    Autocomplete with TAB added; fix ac latency.

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -60,11 +60,16 @@
 ;; Auto-complete
 (require 'ac-cider)
 
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+
 (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
 (add-hook 'cider-mode-hook 'ac-cider-setup)
 (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-(setq ac-delay 0.0)
-(setq ac-quick-help-delay 0.0)
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;;(setq ac-delay 0.0)
+;;(setq ac-quick-help-delay 0.0)
 (eval-after-load "auto-complete"
   '(progn
      (add-to-list 'ac-modes 'cider-mode)
