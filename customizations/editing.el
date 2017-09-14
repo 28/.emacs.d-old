36734ee74a21b7bba3581079bcdaff59565d59f3
Dejan Josifović <Rajoduo@yahoo.com>
2017-09-15 00:50:36 +0200

    Change avy one char binding to avoid conflict; fix error in commands.txt.

--- customizations/editing.el
+++ customizations/editing.el
@@ -60,7 +60,7 @@
 
 ;; Enable avy
 (require 'avy)
-(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-=") 'avy-goto-char)
 (global-set-key (kbd "C-|") 'avy-goto-char-2)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
