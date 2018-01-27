3e4c4d91335e5db783a630caa26436576a63990b
Dejan Josifović <Rajoduo@yahoo.com>
2018-01-27 23:29:21 +0100

    Move avy setup to navigation.

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -61,3 +61,11 @@
 ;; Neotree config
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+
+;; Enable avy
+(require 'avy)
+(global-set-key (kbd "C-=") 'avy-goto-char)
+(global-set-key (kbd "C-|") 'avy-goto-char-2)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
