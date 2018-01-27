e66b13d5a67290e00d0c050addd9df5d61007cd4
Dejan Josifović <Rajoduo@yahoo.com>
2018-01-27 23:30:00 +0100

    Add key bindings for some special characters.

--- customizations/misc.el
+++ customizations/misc.el
@@ -17,3 +17,15 @@
 ;; Org mode
 (global-set-key (kbd "C-c a") 'org-agenda)
 (setq org-log-done t)
+
+;; Bind some special characters
+(global-set-key (kbd "C-x M-l") "λ") ; lambda
+(global-set-key (kbd "C-x M-a") "∧") ; and
+(global-set-key (kbd "C-x M-o") "∨") ; or
+(global-set-key (kbd "C-x M-e") "∈") ; element
+(global-set-key (kbd "C-x M-t") "⊤") ; true
+(global-set-key (kbd "C-x M-b") "⊥") ; bottom
+(global-set-key (kbd "C-x M-f") "∀") ; for all
+(global-set-key (kbd "C-x M-e") "∃") ; there exists
+(global-set-key (kbd "C-x M-n") "¬") ; negation
+(global-set-key (kbd "C-x M-s") "∅") ; empty set
