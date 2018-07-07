d64bf99305cf335f29f57b7b44dac52ccec37cc8
Dejan Josifovic <dejan.josifovic@igt.com>
2018-07-07 22:06:58 +0200

    Add specific file for customizations.

--- init.el
+++ init.el
@@ -160,23 +160,12 @@
 ;; Hard-to-categorize customizations
 (load "misc.el")
 
+;; Specific file for customizations
+(setq custom-file "custom.el") 
+(load custom-file)
+
 ;; Language-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
 (load "setup-web.el")
 (load "setup-elisp.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode avy which-key web-mode magit tagedit rainbow-delimiters projectile smex ido-ubiquitous clj-refactor ac-cider cider clojure-mode-extra-font-locking clojure-mode paredit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
